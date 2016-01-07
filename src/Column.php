2d125dbd784ca191581d68557bb1285934ad7d9a
Gonzalo Ayuso <gonzalo123@gmail.com>
2016-01-07 20:17:00 +0100

    reformat code

--- src/Column.php
+++ src/Column.php
@@ -1,5 +1,6 @@
 <?php
 namespace Pgdbsync;
+
 class Column
 {
     private $_pdo  = null;
@@ -13,20 +14,20 @@ class Column
 
     public function getName()
     {
-        return '"'.$this->_meta['column_name'].'"';
+        return '"' . $this->_meta['column_name'] . '"';
     }
 
     public function getType()
     {
         return $this->_meta['data_type'];
     }
-    
+
     public function getOrder()
     {
         return $this->_meta['ordinal_position'];
     }
-    
-    
+
+
     public function getIsNullable()
     {
         return $this->_meta['is_nullable'] == 'YES' ? true : false;
@@ -35,26 +36,26 @@ class Column
     public function getPrecision()
     {
 
-    	$meta = $this->_meta;
-    	
-    	switch (strtolower($this->_meta['data_type'])) {
-    		case 'character varying':
-    			return $this->_meta['character_maximum_length'];
-			case 'character':
-			    return $this->_meta['character_maximum_length'];
-		    case 'text':
-		        return null;
-    		case 'real':
-			case 'integer':
-    			return null;
-			case 'bigint':
-			    return null;
-    		case 'numeric':
-    			return $this->_meta['numeric_precision'] . ', ' .  $this->_meta['numeric_scale'];
-    			
-    		default:
-    			return null;
-    	}
+        $meta = $this->_meta;
+
+        switch (strtolower($this->_meta['data_type'])) {
+            case 'character varying':
+                return $this->_meta['character_maximum_length'];
+            case 'character':
+                return $this->_meta['character_maximum_length'];
+            case 'text':
+                return null;
+            case 'real':
+            case 'integer':
+                return null;
+            case 'bigint':
+                return null;
+            case 'numeric':
+                return $this->_meta['numeric_precision'] . ', ' . $this->_meta['numeric_scale'];
+
+            default:
+                return null;
+        }
     }
 }
 
--- src/Sequence.php
+++ src/Sequence.php
@@ -1,5 +1,6 @@
 <?php
 namespace Pgdbsync;
+
 class Sequence
 {
     private $_pdo    = null;
@@ -11,66 +12,69 @@ class Sequence
 
     function __construct(\PDO $pdo, $meta, $schema)
     {
-        $this->_pdo   = $pdo;
+        $this->_pdo    = $pdo;
         $this->_schema = $schema;
-        $this->_name  = $meta['relname'];
-        $this->_acl   = $meta['relacl'];
-        $this->_owner = $meta['owner'];
-        $this->_meta  = $this->getSequenceInfo($schema, $this->_name);
+        $this->_name   = $meta['relname'];
+        $this->_acl    = $meta['relacl'];
+        $this->_owner  = $meta['owner'];
+        $this->_meta   = $this->getSequenceInfo($schema, $this->_name);
     }
-    
+
     public function grants()
     {
-    	$out = array();
-    	//{user1=rwU/user1,user2=U/user1}
-    	$acl = $this->_acl;
-    	$acl = str_replace("{", null, $acl);
-    	$acl = str_replace("}", null, $acl);
-    	$aclArr = (array) explode(",", $acl);
-    	foreach ($aclArr as $item) {
-    		$x = explode("=", $item);
-    		$out[] = $x[0];
-    	}
-    	
+        $out = [];
+        //{user1=rwU/user1,user2=U/user1}
+        $acl    = $this->_acl;
+        $acl    = str_replace("{", null, $acl);
+        $acl    = str_replace("}", null, $acl);
+        $aclArr = (array)explode(",", $acl);
+        foreach ($aclArr as $item) {
+            $x     = explode("=", $item);
+            $out[] = $x[0];
+        }
+
         return $out;
     }
-	private function getSequenceInfo($schema, $name)
-	{
-		$out = array();
-		$stmt = $this->_pdo->prepare("SELECT * FROM {$this->_schema}.{$name}");
-		$stmt->execute();	
-		$out = $stmt->fetchAll();
-		return $out[0];
-	}
-	
+
+    private function getSequenceInfo($schema, $name)
+    {
+        $out  = [];
+        $stmt = $this->_pdo->prepare("SELECT * FROM {$this->_schema}.{$name}");
+        $stmt->execute();
+        $out = $stmt->fetchAll();
+
+        return $out[0];
+    }
+
     public function getName()
     {
         return $this->_name;
     }
-    
+
     public function getOwner()
     {
         return $this->_owner;
     }
-    
+
     public function getIncrement()
     {
-		return $this->_meta['increment_by'];
+        return $this->_meta['increment_by'];
     }
-    
+
     public function getMinValue()
     {
-    	return $this->_meta['min_value'];
+        return $this->_meta['min_value'];
     }
-    
+
     public function getMaxValue()
     {
-    	$a = $this->_meta;
-    	return $this->_meta['max_value'];
+        $a = $this->_meta;
+
+        return $this->_meta['max_value'];
     }
-    
+
     public function getStartValue()
     {
-    	return $this->_meta['start_value'];
+        return $this->_meta['start_value'];
     }
 }
