b48e72bc24e0cbd0f16892ae4bc3cce1379ed776
Gonzalo Ayuso <gonzalo123@gmail.com>
2016-01-09 12:42:59 +0100

    class name

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -4,7 +4,7 @@ include __DIR__ . '/fixtures/Database.php';
 
 use Pgdbsync\DbConn;
 
-class ContainerBuilderTest extends \PHPUnit_Framework_TestCase
+class IntegrationTest extends \PHPUnit_Framework_TestCase
 {
     private $database;
     private $conf;
