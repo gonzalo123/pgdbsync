7b83db3aaf56875dd954f67eb84f24d5d1d09f98
Gonzalo Ayuso <gonzalo123@gmail.com>
2016-01-21 14:44:48 +0100

    travis

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -35,7 +35,7 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
 
         $diff    = $dbVc->diff('public');
 
-        $this->assertEquals("Already sync : testdb1\n", $diff);
+        $this->assertEquals("Already sync : db1\n", $diff);
     }
 
     public function test_compare_same_squema_summary()
